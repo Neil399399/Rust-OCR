f6fd7f918a0259eabf77e0abd8b1f3a2098124ea
neil399399 <neil399399@gmail.com>
2020-10-18 16:36:42 +0800

    first push

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Rust-OCR
 Tesseract OCR with Rust
+
+## MAC
+`Homebrew`
+```sh
+brew install tesseract
+```
+
+## OpenCV Bug
+ `--- stderr /usr/local/include/tesseract/publictypes.h:39:1: error: unknown type name 'constexpr' ...`
+ 
+ This is openCV bug (issues 1388)[https://github.com/opencv/opencv_contrib/issues/1388]
---
+++ tesseract/src/main.rs
@@ -0,0 +1,25 @@
+use leptess::{leptonica, tesseract};
+use std::path::Path;
+
+const TEST_IMAGE_URI: &str = "./../test/test_3.png";
+fn main() {
+    let mut api = tesseract::TessApi::new(None, "eng").unwrap();
+
+    let pix = leptonica::pix_read(Path::new(TEST_IMAGE_URI)).unwrap();
+    api.set_image(&pix);
+
+    // detect bounding boxes for words
+    let boxes = api
+        .get_component_images(leptess::capi::TessPageIteratorLevel_RIL_WORD, true)
+        .unwrap();
+
+    println!("Found {} textline image components.", boxes.get_n());
+
+    // run OCR on each word bounding box
+    for b in boxes {
+        api.set_rectangle(&b);
+        let text = api.get_utf8_text().unwrap();
+        let confi = api.mean_text_conf();
+        println!("{:?}, confidence: {}, text: {}", b.get_val(), confi, text);
+    }
+}
